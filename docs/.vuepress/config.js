18552b21dcc63a2204a3a865c5d3b1417c45c468
ooooo <297872913@qq.com>
2020-03-12 16:24:07 +0800

    07、最重要的集群参数配置（上）

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,6 +67,7 @@ module.exports = {
         ['04', '04、Kafka 不同的"发行版"'],
         ['05', '05、Kafka 的版本号'],
         ['06', '06、Kafka 线上部署方案'],
+        ['07', '07、最重要的集群参数配置（上）'],
       ],
       '/bk/redis-development-and-operation-and-maintenance/': [
         '',
