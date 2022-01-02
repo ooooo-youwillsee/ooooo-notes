43b1677fb2f1b2471ca63d290009fab2ddad21d4
ooooo <297872913@qq.com>
2022-01-02 11:26:55 +0800

    2022 plan

--- docs/config/me.js
+++ docs/config/me.js
@@ -1,28 +1,30 @@
 const me = (dir) => '/pages/me' + dir
 
 const nav = {
-  text: 'Me',
-  ariaLabel: 'Me',
-  items: [
-    { text: 'Plan', link: me('/plan/2021') },
-    { text: 'Resolution', link: me('/resolution/2021') }
-  ]
+	text: 'Me',
+	ariaLabel: 'Me',
+	items: [
+		{text: 'Plan', link: me('/plan/2022')},
+		{text: 'Resolution', link: me('/resolution/2022')}
+	]
 }
 
 const sidebar = {
-  [me('/plan/')]: [
-    '',
-    ['2020', '2020 Plan'],
-    ['2021', '2021 Plan']
-  ],
-  [me('/resolution/')]: [
-    ['2019', '2019 Resolution'],
-    ['2020', '2020 Resolution'],
-    ['2021', '2021 Resolution']
-  ]
+	[me('/plan/')]: [
+		'',
+		['2020', '2020 Plan'],
+		['2021', '2021 Plan'],
+		['2022', '2022 Plan']
+	],
+	[me('/resolution/')]: [
+		['2019', '2019 Resolution'],
+		['2020', '2020 Resolution'],
+		['2021', '2021 Resolution'],
+		['2022', '2022 Resolution']
+	]
 }
 
 module.exports = {
-  nav,
-  sidebar
+	nav,
+	sidebar
 }
